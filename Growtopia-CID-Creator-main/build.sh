494830c7a4b3a1c8716dfd5825839c01e7f7f378
aldeeznuts <95154354+aldeeznuts@users.noreply.github.com>
2021-12-06 04:27:18 -0800

    Add files via upload

---
+++ Growtopia-CID-Creator-main/build.sh
@@ -0,0 +1,2 @@
+clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -pthread -std=c++11 -fpermissive -o createid -w #Build source code into executable
+echo -e "Tool succesfuly builded, you can start creating ID by typing ./createid \n\tEdited by github.com/CapciGithub"| boxes -d parchment -a c
